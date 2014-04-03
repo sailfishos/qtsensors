74da857eb9b4bfead5e6c5fd7f6f9476f18c16be
Lorn Potter <lorn.potter@jollamobile.com>
2014-04-03 10:18:43 +1000

    send first proximity signal even when it's "closed".
    This fixes a bug where the proximity sensor would send the initial
    signal only when it was not closed. This will send it regardless.
    
    Change-Id: I5f388e671091ae2a7719f700a049cea41bfaa0db
    Reviewed-by: Alex Blasche <alexander.blasche@digia.com>

--- src/plugins/sensors/sensorfw/sensorfwproximitysensor.cpp
+++ src/plugins/sensors/sensorfw/sensorfwproximitysensor.cpp
@@ -42,12 +42,12 @@
 #include "sensorfwproximitysensor.h"
 
 char const * const SensorfwProximitySensor::id("sensorfw.proximitysensor");
-//bool SensorfwProximitySensor::m_initDone = false;
 
 SensorfwProximitySensor::SensorfwProximitySensor(QSensor *sensor)
     : SensorfwSensorBase(sensor),
       m_initDone(false),
-      m_exClose(false)
+      m_exClose(false),
+      firstRun(true)
 {
     init();
     setReading<QProximityReading>(&m_reading);
@@ -63,7 +63,6 @@ void SensorfwProximitySensor::start()
         Unsigned data(((ProximitySensorChannelInterface*)m_sensorInterface)->proximity());
         m_reading.setClose(data.x()? true: false);
         m_reading.setTimestamp(data.UnsignedData().timestamp_);
-        newReadingAvailable();
     }
     SensorfwSensorBase::start();
 }
@@ -71,11 +70,13 @@ void SensorfwProximitySensor::start()
 void SensorfwProximitySensor::slotDataAvailable(const Unsigned& data)
 {
     bool close = data.x()? true: false;
-    if (close == m_exClose) return;
+    if (!firstRun && close == m_exClose) return;
     m_reading.setClose(close);
     m_reading.setTimestamp(data.UnsignedData().timestamp_);
     newReadingAvailable();
     m_exClose = close;
+    if (firstRun)
+        firstRun = false;
 }
 
 bool SensorfwProximitySensor::doConnect()
